e35e391beb40c4de3d4278f988c0eca7ba5f7c54
ttom921 <ttom921@hotmail.com>
2019-08-12 12:26:28 +0800

    更新資料

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Ironman1612
-你懂 JavaScript 嗎？ 
+這是參考這是參考2019 iT 邦幫忙鐵人賽的[你懂 JavaScript 嗎？](https://ithelp.ithome.com.tw/users/20092232/ironman/1612)這篇文章來學習和記錄
+
+[github部落格](https://ttom921.github.io/學習你懂JavaScirpt嗎/)
+
---
+++ javascript02.js
@@ -0,0 +1,21 @@
+// const x = 1;
+// y = 2;
+// const product = {
+//     name: 'apple',
+//     count: 100,
+// };
+
+// delete x;
+// console.log(x);
+// delete y;
+// //console.log(y);
+
+// delete product.count;
+// console.log(product);
+
+const product ={
+    name:'apple',
+    count:100,
+}
+console.log('name' in product);
+console.log('vaild' in product);
